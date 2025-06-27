f47e9ea8cd07007986758b456d893eacf84f7c89
LukeyG07 <133875150+LukeyG07@users.noreply.github.com>
2025-06-27 21:45:26 +1000

    Create db.js

---
+++ db.js
@@ -0,0 +1,14 @@
+// db.js: PostgreSQL client setup & initial schema run
+const { Client } = require('pg');
+const fs = require('fs');
+require('dotenv').config();
+
+const client = new Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+
+async function init() {
+  await client.connect();
+  const schema = fs.readFileSync('./schema.sql').toString();
+  await client.query(schema);
+}
+
+module.exports = { client, init };
