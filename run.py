f12181c275a8c0db509a9e09bbfe784f86f2c2c5
LukeyG07 <133875150+LukeyG07@users.noreply.github.com>
2025-06-29 20:48:20 +1000

    Update run.py

--- run.py
+++ run.py
@@ -1,32 +1,8 @@
 import os
-from flask import Flask
-from config import Config
-from extensions import db, migrate, login
+from app import create_app
 
-# create app
-app = Flask(__name__, template_folder='.', static_folder='.', static_url_path='')
-app.config.from_object(Config)
+app = create_app()
 
-# initialize extensions with app
-db.init_app(app)
-migrate.init_app(app, db)
-login.init_app(app)
-
-# import modules that use extensions
-import models  # noqa: F401
-import routes  # noqa: F401
-
-# initialize DB & create default admin
-with app.app_context():
-    db.create_all()
-    from models import User
-    if not User.query.filter_by(username='admin').first():
-        admin = User(username='admin', is_admin=True)
-        admin.set_password('password')
-        db.session.add(admin)
-        db.session.commit()
-
-# run server
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
